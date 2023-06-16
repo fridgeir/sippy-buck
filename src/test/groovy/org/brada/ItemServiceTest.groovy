3e3414f072395f9161ab65b88bf861620c53a4aa
Fridgeir <kkrzysztof16@wp.pl>
2023-06-16 13:06:19 +0200

    init no spring app

---
+++ src/test/groovy/org/brada/ItemServiceTest.groovy
@@ -0,0 +1,22 @@
+package org.brada
+
+import org.brada.sippy.domain.port.ItemService
+import spock.lang.Specification
+
+class ItemServiceTest extends Specification {
+
+    ItemService service
+
+    def setup() {
+        service = new ItemService()
+    }
+
+    def "test we cannot register signle instance with incorrect address"() {
+        when:
+        def item = service.getItem("1")
+
+        then:
+        item.name() == "name"
+    }
+
+}
